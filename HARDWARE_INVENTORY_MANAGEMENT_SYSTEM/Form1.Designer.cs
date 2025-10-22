461f02ef135d67d3c212774762029aad765a9ad7
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-10-22 22:52:34 +0800

    Testing

--- HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Form1.Designer.cs
+++ HARDWARE_INVENTORY_MANAGEMENT_SYSTEM/Form1.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlSideBar = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
-            ((System.ComponentModel.ISupportInitialize)(this.pnlSideBar)).BeginInit();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.SuspendLayout();
             // 
-            // pnlSideBar
+            // guna2Panel1
             // 
-            this.pnlSideBar.Location = new System.Drawing.Point(23, 26);
-            this.pnlSideBar.Name = "pnlSideBar";
-            this.pnlSideBar.Size = new System.Drawing.Size(198, 698);
-            this.pnlSideBar.StateCommon.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(222)))), ((int)(((byte)(239)))));
-            this.pnlSideBar.StateCommon.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(110)))), ((int)(((byte)(196)))), ((int)(((byte)(20)))));
-            this.pnlSideBar.TabIndex = 0;
+            this.guna2Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.Location = new System.Drawing.Point(24, 26);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(198, 698);
+            this.guna2Panel1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -48,19 +47,17 @@
             this.BackgroundImage = global::HARDWARE_INVENTORY_MANAGEMENT_SYSTEM.Properties.Resources._05_Dashboard;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1200, 749);
-            this.Controls.Add(this.pnlSideBar);
+            this.Controls.Add(this.guna2Panel1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pnlSideBar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private ComponentFactory.Krypton.Toolkit.KryptonPanel pnlSideBar;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
 
